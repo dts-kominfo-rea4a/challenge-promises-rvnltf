678d8769797efa932054b89d338f29522f049c01
A. Rivan M. Latief <aldirivan56@gmail.com>
2022-09-16 10:43:18 +0700

    Tantangan: Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (q) => {
+  try {
+    const IXX = await promiseTheaterIXX();
+    const VGC = await promiseTheaterVGC();
+
+    const data = [...IXX, ...VGC];
+
+    return data.filter((v) => v.hasil == q).length;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
